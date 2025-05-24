affcf81530363946b376546382e4da31bf2bd47e
Lowl. <about.lowly@gmail.com>
2025-05-25 00:53:50 +0700

    button color has fixed

--- scripts/julfix.user.js
+++ scripts/julfix.user.js
@@ -79,8 +79,7 @@
             right: 20px;
             z-index: 9999;
             padding: 10px 15px;
-            background-color: #00000;
-            opacity: 50%;
+            background-color:rgba(0, 0, 0, 0.5);
             color: white;
             border: none;
             border-radius: 5px;
